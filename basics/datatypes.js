9ec6230e9e9b56b9ce568f03fcdada0149aa92d4
hariom chaudhary <hariomchaudhary1795@gmail.com>
2026-04-13 23:13:02 +0530

    add

---
+++ basics/datatypes.js
@@ -0,0 +1,12 @@
+console.log(3+3)
+let name="hariom"//stringdatatype
+let age=22 //nuberdatatype
+let IslogedIn=false //bolean
+let state;
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof state)
+console.log(typeof IslogedIn)
+
+
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=12345
+console.log(accountId)
+let accountEmail="asbhsbqh@gmail.com"
+console.log(accountEmail)
+let accountState;
+let accountPassword="12233434"
+let accountCity="delhi"
+console.log(accountState)
+// const accountId=2  <not allowed
+//console.log(accountId)
+ accountEmail="hshash@gamiaksnscom"
+console.log(accountEmail)
+console.log([accountId,accountEmail,accountPassword,accountCity])
