8940454f64119dc315a09433c454bebeae589da8
hariom chaudhary <hariomchaudhary1795@gmail.com>
2026-04-16 17:44:18 +0530

    update

---
+++ basics/conversionandoperations.js
@@ -0,0 +1,38 @@
+let Score="30"
+console.log(typeof Score);
+console.log(typeof (Score));
+let ValueInNumber=Number(Score);//string to Number
+console.log(typeof ValueInNumber)
+let isLoggedin=1;
+let BooleanisLoggedin=Boolean(isLoggedin)
+console.log(BooleanisLoggedin)
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(StringNumber)
+
+//operations
+
+let Value=3
+let NegativeValue=-Value
+console.log(NegativeValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+let str1="hariom"
+let str2=" chaudhary"
+let str3=str1+str2
+console.log(str3)
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+
+
+
+
+
---
+++ basics/datatypes2.js
@@ -0,0 +1,56 @@
+//comparision
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==1);
+// console.log(null>=0);
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+// //datatypes
+// const Score=100; //number
+// const Scorevalue=90.9 //number
+// const isLoggedin=false //boolean
+// const outsideTemp=null //null
+// let Useremail; //undefined
+// const id=Symbol('123');
+// const Anotherid=Symbol('123')
+// console.log(id===Anotherid);
+// //array
+// const heros=["as","as","ass"]
+// //object
+// let myObj={
+//     name:"hariom",
+// age:22;
+// }
+//const myFunction=function(){ 
+   // console.log("hello world");
+//}
+//@@ STACK & HEAP
+
+let myyoutubename="hkhub"
+let myanothername="myyoutubename"
+anothername="chaiaurcode"
+console.log(myyoutubename);
+console.log(anothername);
+
+//@@heap
+
+let userOne={
+    email:"asas@.com",
+    upi:"user@ybl"
+
+}
+let userTwo=userOne
+userTwo.email="asdqw@d.com"
+console.log(userOne.email)
+console.log(userTwo.email)
+
+
+
+
